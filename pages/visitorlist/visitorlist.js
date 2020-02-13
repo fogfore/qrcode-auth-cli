73b732e3628c5de9725c97b6f0a484b26ff958d8
fogfore <yh.924@foxmail.com>
2020-02-13 13:50:45 +0800

    添加访问者 删除访问者

--- pages/visitorlist/visitorlist.js
+++ pages/visitorlist/visitorlist.js
@@ -5,7 +5,8 @@ const baseUrl = app.globalData.baseUrl
 Page({
   data: {
     addrId: null,
-    addrName: null
+    addrName: null,
+    visitors: []
   },
   onLoad: function (options) {
     this.setData({
@@ -15,5 +16,56 @@ Page({
     wx.setNavigationBarTitle({
       title: options.addrName,
     })
+    let that = this
+    wx.request({
+      url: baseUrl + '/user/list/visitors/' + options.addrId,
+      header: {
+        skey: wx.getStorageSync('skey')
+      },
+      success: function (res) {
+        console.log(res)
+        if (res.data.status == '200') {
+          let data = res.data.data
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].addrAuth == 1) {
+              data[i].slideBtns = [{
+                text: '删除',
+                data: data[i].id
+              }]
+            }
+          }
+          console.log(data)
+          that.setData({
+            visitors: data
+          })
+        }
+      }
+    })
   },
+  slideButtonTap: function (e) {
+    console.log(e)
+    let that = this
+    wx.request({
+      url: baseUrl + '/user/del/visitor',
+      method: 'POST',
+      header: {
+        skey: wx.getStorageSync('skey')
+      },
+      data: {
+        addrId: this.data.addrId,
+        visitorId: e.detail.data
+      },
+      success: function (res) {
+        if (res.data.status == '200') {
+          wx.showToast({
+            title: '删除成功',
+          })
+          that.onLoad({
+            addrId: that.data.addrId,
+            addrName: that.data.addrName
+          })
+        }
+      }
+    })
+  }
 })
